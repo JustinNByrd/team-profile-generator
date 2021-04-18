91755504f0e988fd3461f1363139974beb25e686
Justin Byrd <justin_byrd@hotmail.com>
2021-04-18 17:42:39 -0400

    Coded Engineer class in ./utils/engineer.js

---
+++ utils/engineer.js
@@ -0,0 +1,8 @@
+class Engineer extends Employee {
+    constructor(name, empID, email, gitHubUsername) {
+        super(name, empID, email);
+        this.gitHubUsername = gitHubUsername;
+    }
+}
+
+module.exports = Engineer;
