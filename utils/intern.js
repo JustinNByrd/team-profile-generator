2adf14c92c6511e4ea40fc83b2b62f0b1b7ec000
Justin Byrd <justin_byrd@hotmail.com>
2021-04-18 17:44:38 -0400

    Coded Intern class in ./utils/intern.js

---
+++ utils/intern.js
@@ -0,0 +1,8 @@
+class Intern extends Employee {
+    constructor(name, empID, email, school) {
+        super(name, empID, email);
+        this.school = school;
+    }
+}
+
+module.exports = Intern;
