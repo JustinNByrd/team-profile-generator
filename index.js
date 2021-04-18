5fdb0e403432b9b1f637307d1703d0d51624d35f
Justin Byrd <justin_byrd@hotmail.com>
2021-04-18 18:00:12 -0400

    Coded requires in index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+// include the inquirer and filesystem packages
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// include the ./utils files
+const Employee = require('./utils/employee');
+const Intern = require('./utils/intern');
+const Engineer = require('./utils/engineer');
+const Manager = require('./utils/manager');
+
