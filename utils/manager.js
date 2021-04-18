d2df9207d35bfb1ace5c3f3f704801cde77def66
Justin Byrd <justin_byrd@hotmail.com>
2021-04-18 17:45:52 -0400

    Coded Manager class in ./utils/manager.js

---
+++ utils/manager.js
@@ -0,0 +1,8 @@
+class Manager extends Employee {
+    constructor(name, empID, email, officeNum) {
+        super(name, empID, email);
+        this.officeNum = officeNum;
+    }
+}
+
+module.exports = Manager;
