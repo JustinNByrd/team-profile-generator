e5c974ffee544c0d012f67393bcc9cc97751e3c6
Justin Byrd <justin_byrd@hotmail.com>
2021-04-24 19:40:37 -0400

    Built out generateHTML.js

--- utils/generateHTML.js
+++ utils/generateHTML.js
@@ -1,6 +1,46 @@
 function generateHTML(teamName, empArr) {
-    let generatedHTML = `teamName = ${teamName}\n`;
-    generatedHTML += `empArr = ${empArr}\n`
+    let generatedHTML = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Team Profile</title>
+</head>
+<body>
+<header>
+    <h1>${teamName}</h1>
+</header>
+<main>
+`;
+
+    empArr.forEach(emp => {
+        generatedHTML += `<div class="${emp.constructor.name.toLowerCase()}">
+<h3>${emp.name}</h3>
+<h4>${emp.constructor.name}</h4>
+<p>ID: ${emp.empID}</p>
+<p>Email: <a href="mailto:${emp.email}">${emp.email}</p>`
+
+        switch (emp.constructor.name) {
+            case 'Manager':
+                generatedHTML += `<p>Office: ${emp.officeNum}</p>`;
+                break;
+            case 'Engineer':
+                generatedHTML += `<p>GitHub: <a href="https://github.com/${emp.gitHubUsername}">${emp.gitHubUsername}</a></p>`;
+                break;
+            case 'Intern':
+                generatedHTML += `<p>School: ${emp.school}</p>`;
+                break;
+        }
+        generatedHTML += `</div>`;
+    });
+
+    generatedHTML += `</main>
+</body>
+</html>`;
+
+
+    return generatedHTML;
 }
 
 module.exports = generateHTML;
