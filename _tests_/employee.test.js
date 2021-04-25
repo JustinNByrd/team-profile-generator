572cf86a23878acf9deae3c7f07e4f7e2eec882b
Justin Byrd <justin_byrd@hotmail.com>
2021-04-24 23:27:42 -0400

    Added tests in ./_tests_/employee.test.js

---
+++ _tests_/employee.test.js
@@ -0,0 +1,30 @@
+const Employee = require("../utils/employee");
+
+describe("Employee", () => {
+    describe("Initialization", () => {
+        it("should return an object of type employee.", () => {
+            const obj = new Employee('Justin', 1, 'justin@here.com',);
+            expect(obj instanceof Employee).toEqual(true);
+        })
+
+        it("Should return the employee's name", () => {
+            const obj = new Employee('Justin', 1, 'justin@here.com',);
+            expect(obj.getName()).toEqual("Justin");
+        })
+
+        it("Should return the employee's ID number", () => {
+            const obj = new Employee('Justin', 1, 'justin@here.com',);
+            expect(obj.getID()).toEqual(1);
+        })
+
+        it("Should return the employee's email", () => {
+            const obj = new Employee('Justin', 1, 'justin@here.com',);
+            expect(obj.getEmail()).toEqual("justin@here.com");
+        })
+
+        it("Should return the employee's role", () => {
+            const obj = new Employee('Justin', 1, 'justin@here.com',);
+            expect(obj.getRole()).toEqual("Employee");
+        })
+    })
+});
